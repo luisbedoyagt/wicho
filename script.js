071cd5914bea041a51140f768e49655bf0cd87df
luisbedoyagt <90284786+luisbedoyagt@users.noreply.github.com>
2025-09-04 10:52:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function factorial(n) {
 // ----------------------
 // CONFIGURACIÓN DE LIGAS
 // ----------------------
-const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzRX7K7XwzhgIR_gLFdaOzKpykGqliDKD1a19-EmKiVEobWzCQ1_RZz1_Lhl8pDvfAADQ/exec";
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbx_AzBaKo_iRz2CJwExVO_DXnVcjN4c3Gu1n6nZKkMUj4OwsxSgXPslLoTAguFeEXd2/exec";
 let teamsByLeague = {};
 let allData = {};
 let currentEventPage = 0;
@@ -923,4 +923,5 @@ function calculateAll() {
     const combinedPrediction = getCombinedPrediction(stats, event?.pronostico, matchData);
     $('combined-prediction').innerHTML = `<div class="combined-box"><h3>${combinedPrediction.header}</h3><div class="combined-body">${combinedPrediction.body}</div></div>`;
 }
+
 
